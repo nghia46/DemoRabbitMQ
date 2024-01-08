b3027ad5d6bdf18e40b7ddb8ed5c9ca6c14a27ca
nghia46 <nghialoe46@gmail.com>
2024-01-08 22:01:11 +0700

    init

---
+++ ConsumerService/ConsumerService/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "message_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine($" [x] Received '{message}'");
+    };
+
+    channel.BasicConsume(queue: "message_queue", autoAck: true, consumer: consumer);
+
+    Console.WriteLine("Press [Enter] to exit.");
+    Console.ReadLine();
+}
---
+++ PublisherService/PublisherService/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "message_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    string message = "Hello, Nghia";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "", routingKey: "message_queue", basicProperties: null, body: body);
+
+    Console.WriteLine($" [x] Sent '{message}'");
+}
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
